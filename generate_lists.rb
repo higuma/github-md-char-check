eeccb0df05eaeba64ff1a92a127e8957a29e9b4c
Yuji Miyane <myuj1964@gmail.com>
2022-09-27 11:30:58 +0900

    fix for special cases (backslash and backtick)

--- generate_lists.rb
+++ generate_lists.rb
@@ -5,9 +5,7 @@
 N_COL = 16
 
 def output_hr(f)
-  f.puts
-  f.puts "--------------------"
-  f.puts
+  f.puts "\n--------------------\n\n"
 end
 
 def output_links(f, code_from)
@@ -27,19 +25,23 @@ def output_links(f, code_from)
   f.puts
 end
 
+def output_cell(code)
+  chr = code.chr(Encoding::UTF_8)
+  code_span = chr == "`" ?  "`` ` ``" : "`#{chr}`" 
+  chr = "\\#{chr}" if chr == "\\" || chr == "`"
+  sprintf(" <span id=\"%04X\">%s</span><br>%s<br>%s\uFE0E<br>%s\uFE0F |",
+          code, code_span, chr, chr, chr)
+end
+
 def generate_char_list(f, code_from, code_to)
-  f.printf "# Markdown outputs for Unicode characters (U+%04X..%04X)\n", code_from, code_to
-  f.puts
+  f.printf "# Markdown outputs for Unicode characters (U+%04X..U+%04X)\n\n", code_from, code_to
   code_base = code_from
   output_links f, code_base
   output_hr f
-
-  f.puts "`c`: as code span, &nbsp; **c**: character only, &nbsp; **15**: character + VS15(U+FE0E), &nbsp; **16**: character + VS16(U+FE0F)"
-  f.puts
+  f.puts "`c`: as code span, &nbsp; **c**: character only, &nbsp; **15**: character + VS15(U+FE0E), &nbsp; **16**: character + VS16(U+FE0F)\n\n"
 
   if code_from < 0x20
-    f.puts "> U+0000..001F are omitted (unprintable control characters)."
-    f.puts
+    f.puts "> U+0000..U+001F are omitted (unprintable control characters).\n\n"
     code_from = 0x20
   end
 
@@ -51,18 +53,12 @@ def generate_char_list(f, code_from, code_to)
   until all_codes.empty?
     codes = all_codes.shift(N_COL)
     if codes[0] == 0xD800
-      f.puts
-      f.puts "> U+D800..DFFF are omitted (reserved for surrogate pairs)."
+      f.puts "\n> U+D800..U+DFFF are omitted (reserved for surrogate pairs)."
       break
-    else
-      hex = sprintf("%03X", codes[0] / 16)
-      f.puts "| <span id=\"#{hex}x\">#{hex}0</span> |" +
-             codes.inject('') {|row, code|
-               chr = code.chr(Encoding::UTF_8)
-               row + sprintf(" <span id=\"%04X\">`%s`</span><br>%s<br>%s\uFE0E<br>%s\uFE0F |",
-                             code, chr, chr, chr, chr)
-             }
     end
+    hex = sprintf("%03X", codes[0] / 16)
+    f.puts "| <span id=\"#{hex}x\">#{hex}0</span> |" +
+           codes.inject('') {|row, code| row + output_cell(code) }
   end
   output_hr f
   output_links f, code_base
